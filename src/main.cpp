97f89fff28a529b618e4025000ffbab662f482c5
rin <117419794+rin-cast-9@users.noreply.github.com>
2024-10-09 17:40:04 +0800

    feat: config

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
+#include <string>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+
+
+struct Config {
+    double step;
+    std::string function;
+    int population_size;
+    double mutation_rate;
+};
+
+Config parseConfig(const std::string & filePath) {
+    Config config;
+    boost::property_tree::ptree ptree;
+    boost::property_tree::ini_parser::read_ini(filePath, ptree);
+
+    config.step = ptree.get <double> ("step");
+    config.function = ptree.get <std::string> ("function");
+    config.population_size = ptree.get <int> ("population_size");
+    config.mutation_rate = ptree.get <double> ("mutation_rate");
+
+    return config;
+}
+
+void printConfig(const Config & config) {
+    std::cout << "step: " << config.step << '\n';
+    std::cout << "function: " << config.function << '\n';
+    std::cout << "population size: " << config.population_size << '\n';
+    std::cout << "mutation rate: " << config.mutation_rate << '\n';
+}
 
 
 int main() {
 
     std::cout << "hello, ga program kokokara desu!\n";
+
+    Config config = parseConfig("../config.ini");
+    printConfig(config);
 
     return 0;
 
